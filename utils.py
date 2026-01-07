e13f7cbf118e710de1f4b4ec0b3faca5ad5d65a7
khaild100bot-prog <khaild100bot@gmail.com>
2026-01-07 07:29:03 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,48 +1,32 @@
+
 import re
+import os
 from uuid import uuid4
-
 from yt_dlp import YoutubeDL
-
 
 def is_video_url(url):
     pattern = re.compile(
-        r'(https?://)?(www\.)?(fb\.watch|youtube\.com|youtu\.?be|facebook\.com|twitter\.com|x\.com|instagram\.com|reddit\.com)/.+')
+        r'(https?://)?(www\.)?(tiktok\.com|instagram\.com|youtube\.com|youtu\.?be|facebook\.com|fb\.watch|twitter\.com|x\.com|reddit\.com)/.+'
+    )
     return bool(pattern.match(url))
 
-
-def get_video_info(url):
-    with YoutubeDL() as ydl:
-        info = ydl.extract_info(url, download=False)
-        sanitized_info = ydl.sanitize_info(info)
-        return sanitized_info
-
-
-def get_file_size(sanitized_info):
-    if 'filesize_approx' in sanitized_info:
-        file_size = sanitized_info['filesize_approx']
-        if file_size is None:
-            raise Exception('File size not found')
-        return file_size
-    else:
-        raise Exception('File size not found')
-
-
-def get_duration(sanitized_info):
-    if 'duration' in sanitized_info:
-        duration = sanitized_info['duration']
-        if duration is None:
-            raise Exception('Duration not found')
-        return duration
-    else:
-        raise Exception('Duration not found')
-
-
 def download(url):
-    uuid = str(uuid4())
-
+    unique_id = str(uuid4())
+    output_dir = 'downloads'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        
+    file_path = os.path.join(output_dir, f"{unique_id}.%(ext)s")
+    
     ydl_opts = {
-        'outtmpl': f'/tmp/{uuid}.mp4',
+        'format': 'best',
+        'outtmpl': file_path,
+        'noplaylist': True,
+        'quiet': True,
+        'no_warnings': True,
     }
+    
     with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-    return uuid
+        info = ydl.extract_info(url, download=True)
+        actual_filename = ydl.prepare_filename(info)
+        return actual_filename
